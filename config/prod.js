fab8aac85c999aa4fe1faf5ecb38863e2bcdf749
timlee0119 <b05705006@ntu.edu.tw>
2020-05-25 14:27:31 +0800

    update prod keys

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '354566823383-smdg3t4gpqj2j92oai4j697dr4o3rs4i.apps.googleusercontent.com',
+  googleClientSecret: 'dCwj0mjbZKHU0Mad9XoYodoH',
+  mongoURI:
+    'mongodb+srv://timlee0119:T!mzhps30153@dev-cluster-9vfa5.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'leehengencoolhaha',
+  extensionOrigin: 'chrome-extension://gjmkckfpkokecadhhmfhafdnkfppbdlj'
+};
