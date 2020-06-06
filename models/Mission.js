2c50813c777d29cff2aa6326f717383c9a44b253
timlee0119 <b05705006@ntu.edu.tw>
2020-06-07 02:10:42 +0800

    Mission model skip Partitipants versioning

--- models/Mission.js
+++ models/Mission.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose');
 const ParticipantSchema = require('./Participant').schema;
 
-const missionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  days: { type: Number, required: true },
-  money: { type: Number, required: true },
-  participants: {
-    type: [ParticipantSchema],
-    validate: [validateParticipants, 'Participants length should be 1 or 2']
+const missionSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    days: { type: Number, required: true },
+    money: { type: Number, required: true },
+    participants: {
+      type: [ParticipantSchema],
+      validate: [validateParticipants, 'Participants length should be 1 or 2']
+    },
+    code: {
+      type: String,
+      default: function () {
+        var buf = new Buffer.from(String(this._id));
+        return buf.toString('base64');
+      }
+    },
+    startTime: { type: Date }
   },
-  code: {
-    type: String,
-    default: function () {
-      var buf = new Buffer.from(String(this._id));
-      return buf.toString('base64');
+  {
+    skipVersioning: {
+      participants: true
     }
-  },
-  startTime: { type: Date }
-});
+  }
+);
 
 function validateParticipants(val) {
   return val.length >= 1 && val.length <= 2;
